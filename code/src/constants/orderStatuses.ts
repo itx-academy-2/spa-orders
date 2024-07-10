8b41017ac1013f753cd6526c5db22c1c0291803b
Eugene <eugene.hetsyanyn@gmail.com>
2024-07-10 11:54:58 +0000

    Created orders table (#238)
    * Sync manager role with backend side (#237)
    
    * Sync manager role with backend side
    
    * Fix role
    
    * added tests
    
    * added translations
    
    * add translations for statues. Small changes
    
    ---------
    
    Co-authored-by: Maksym Hevyk <109922101+mhevyk@users.noreply.github.com>

--- code/src/constants/orderStatuses.ts
+++ code/src/constants/orderStatuses.ts
@@ -1,7 +1,7 @@
-export const orderStatuses = {
-  IN_PROGRESS: "in progress",
-  SHIPPED: "shipped",
-  DELIVERED: "delivered",
-  CANCELED: "canceled",
-  COMPLETED: "completed"
+export const orderStatusesTranslationKeys = {
+  IN_PROGRESS: "orders.statuses.inProgress",
+  SHIPPED: "orders.statuses.shipped",
+  DELIVERED: "orders.statuses.delivered",
+  CANCELED: "orders.statuses.canceled",
+  COMPLETED: "orders.statuses.completed"
 } as const;
