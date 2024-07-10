dd0ae75c408eb275e3d1e8d293e452c54bacf80a
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-07-10 12:05:13 +0300

    Created cart drawer (#243)
    * Created cart drawer

--- code/src/components/product-card/ProductCard.tsx
+++ code/src/components/product-card/ProductCard.tsx
@@ -4,20 +4,20 @@ import AppBox from "@/components/app-box/AppBox";
 import AppButton from "@/components/app-button/AppButton";
 import AppLink from "@/components/app-link/AppLink";
 import AppTypography from "@/components/app-typography/AppTypography";
+import { ProductCardProps } from "@/components/product-card/ProductCard.types";
 
-import { Product } from "@/types/product.types";
 import formatPrice from "@/utils/format-price/formatPrice";
 
 import "@/components/product-card/ProductCard.scss";
 
-type ProductCardProps = {
-  product: Product;
-};
+const ProductCard = ({ product, onAddToCart }: ProductCardProps) => {
+  const handleAddToCart = () => {
+    onAddToCart(product);
+  };
 
-const ProductCard = ({ product }: ProductCardProps) => {
   return (
-    <AppLink key={product.id} to="/">
-      <AppBox className="spa-product-card">
+    <AppBox className="spa-product-card">
+      <AppLink className="spa-product-card__link-wrapper" to="/">
         <AppBox className="spa-product-card__img">
           <AppBox
             alt={product.name}
@@ -32,17 +32,17 @@ const ProductCard = ({ product }: ProductCardProps) => {
         <AppBox>
           <AppTypography variant="caption">{product.name}</AppTypography>
         </AppBox>
-        <AppBox className="spa-product-card__footer">
-          <AppTypography className="spa-product-card__footer-price">
-            {formatPrice(product.price)}
-          </AppTypography>
-          <AppButton size="small" variant="shadow">
-            <AddIcon />
-            <AppTypography translationKey="productCard.add" />
-          </AppButton>
-        </AppBox>
+      </AppLink>
+      <AppBox className="spa-product-card__footer">
+        <AppTypography className="spa-product-card__footer-price">
+          {formatPrice(product.price)}
+        </AppTypography>
+        <AppButton size="small" variant="shadow" onClick={handleAddToCart}>
+          <AddIcon />
+          <AppTypography translationKey="productCard.add" />
+        </AppButton>
       </AppBox>
-    </AppLink>
+    </AppBox>
   );
 };
 
---
+++ code/src/layouts/cart-drawer/message/index.ts
@@ -0,0 +1,4 @@
+import en from "@/layouts/cart-drawer/message/en.json";
+import uk from "@/layouts/cart-drawer/message/uk.json";
+
+export default { uk, en };
