5c7d38180578704011d56b71e3163658e9c35d43
OliaHrytsak <127191734+OliaHrytsak@users.noreply.github.com>
2024-08-02 16:38:37 +0300

    Edit admin layout (#440)
    * adit admin layout
    
    * changed tabs position

--- code/src/layouts/dashboard-layout/components/dashboard-admin-tab-layout/DashboardAdminLayout.constants.tsx
+++ code/src/layouts/dashboard-layout/components/dashboard-admin-tab-layout/DashboardAdminLayout.constants.tsx
@@ -1,4 +1,5 @@
 import PeopleIcon from "@mui/icons-material/People";
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 
 import { DASHBOARD_TAB_NAMES } from "@/layouts/dashboard-layout/DashboardLayout.constants";
 import { DashboardTab } from "@/layouts/dashboard-layout/DashboardLayout.types";
@@ -8,5 +9,10 @@ export const dashboardAdminTabs: DashboardTab[] = [
     labelTranslationKey: "dashboardTabs.users.label",
     name: DASHBOARD_TAB_NAMES.USERS,
     icon: <PeopleIcon />
+  },
+  {
+    labelTranslationKey: "dashboardTabs.orders.label",
+    name: DASHBOARD_TAB_NAMES.ORDERS,
+    icon: <ReceiptLongIcon />
   }
 ];
