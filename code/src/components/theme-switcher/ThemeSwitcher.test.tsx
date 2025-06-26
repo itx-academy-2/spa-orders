546e8041b55c1726d571f1f24867b506d4f21266
Daniil Bokach <daniilbokach@gmail.com>
2025-06-26 18:22:34 +0300

    fix(test): remove code duplication

--- code/src/components/theme-switcher/ThemeSwitcher.test.tsx
+++ code/src/components/theme-switcher/ThemeSwitcher.test.tsx
@@ -3,20 +3,21 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 describe("ThemeSwitcher", () => {
-  it("should render the switch component", () => {
+  beforeEach(() => {
     render(<ThemeSwitcher />);
+  });
+
+  it("should render the switch component", () => {
     const switchElement = screen.getByRole("checkbox");
     expect(switchElement).toBeInTheDocument();
   });
 
   it("should not be checked by default", () => {
-    render(<ThemeSwitcher />);
     const switchElement = screen.getByRole("checkbox") as HTMLInputElement;
     expect(switchElement.checked).toBe(false);
   });
 
   it("should toggle checked state when clicked", () => {
-    render(<ThemeSwitcher />);
     const switchElement = screen.getByRole("checkbox") as HTMLInputElement;
 
     fireEvent.click(switchElement);
