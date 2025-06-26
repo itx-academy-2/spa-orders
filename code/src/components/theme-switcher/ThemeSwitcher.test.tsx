b830e808b6f16a20bc68f738d0546106b1c45ff3
Daniil Bokach <daniilbokach@gmail.com>
2025-06-26 18:01:14 +0300

    fix(test): rename test cases

--- code/src/components/theme-switcher/ThemeSwitcher.test.tsx
+++ code/src/components/theme-switcher/ThemeSwitcher.test.tsx
@@ -3,19 +3,19 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 describe("ThemeSwitcher", () => {
-  it("renders the switch component", () => {
+  it("should render the switch component", () => {
     render(<ThemeSwitcher />);
     const switchElement = screen.getByRole("checkbox");
     expect(switchElement).toBeInTheDocument();
   });
 
-  it("is not checked by default", () => {
+  it("should not be checked by default", () => {
     render(<ThemeSwitcher />);
     const switchElement = screen.getByRole("checkbox") as HTMLInputElement;
     expect(switchElement.checked).toBe(false);
   });
 
-  it("toggles checked state when clicked", () => {
+  it("should toggle checked state when clicked", () => {
     render(<ThemeSwitcher />);
     const switchElement = screen.getByRole("checkbox") as HTMLInputElement;
 
