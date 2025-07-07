9691b83d3f171d5cf7eaa1c58d415b64fdcc3eb5
Daniil Bokach <daniilbokach@gmail.com>
2025-07-07 17:41:17 +0300

    fix(test): remove unnecessary renders

--- code/src/components/account-page/AccountTitle.test.tsx
+++ code/src/components/account-page/AccountTitle.test.tsx
@@ -3,24 +3,22 @@ import { render, screen } from "@testing-library/react";
 import AccountTitle from "./AccountTitle";
 
 describe("AccountTitle", () => {
-  it("should render title", () => {
-    render(<AccountTitle title="Customer Account" />);
-    expect(screen.getByText("Customer Account")).toBeInTheDocument();
-  });
-
-  it("should render description when provided", () => {
+  it("should render title and optional description when provided", () => {
     render(
       <AccountTitle
         title="Customer Account"
         description="Manage your account"
       />
     );
+
+    expect(screen.getByText("Customer Account")).toBeInTheDocument();
     expect(screen.getByText("Manage your account")).toBeInTheDocument();
   });
 
   it("should not render description when not provided", () => {
     render(<AccountTitle title="Customer Account" />);
-    const description = screen.queryByText("Manage your account");
-    expect(description).not.toBeInTheDocument();
+
+    expect(screen.getByText("Customer Account")).toBeInTheDocument();
+    expect(screen.queryByText("Manage your account")).not.toBeInTheDocument();
   });
 });
