8823a133c7a60290d7f5f1da8265073ae3564446
Sofiia13 <sofiia.yevush@gmail.com>
2025-07-14 23:52:46 +0300

    added translations

---
+++ code/src/components/profile-personal-information/messages/index.ts
@@ -0,0 +1,4 @@
+import en from "@/components/profile-personal-information/messages/en.json";
+import uk from "@/components/profile-personal-information/messages/uk.json";
+
+export default { en, uk };
