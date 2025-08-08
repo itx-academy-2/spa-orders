bdc2357ba3bfa13c6aeb9bd0efb2344574616a44
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2025-08-08 14:38:37 +0300

    refactored

--- e2e/cypress/test_module/cypress/support/commands.ts
+++ e2e/cypress/test_module/cypress/support/commands.ts
@@ -43,7 +43,9 @@ Cypress.Commands.add("loginWithRole", (role = "ROLE_USER") => {
   const email = Cypress.env(`${role}_EMAIL`);
   const password = Cypress.env(`${role}_PASSWORD`);
 
-  cy.intercept("POST", "/api/auth/sign-in").as("loginRequest");
+  cy.intercept( httpMethod.post, "**/auth/sign-in*" ).as(
+    "loginRequest"
+  );
 
   cy.visitWithLanguage("/");
   cy.getById("auth-button").click();
