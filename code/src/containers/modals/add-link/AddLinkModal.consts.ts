7be7483dc9b9866c564c6c833f8f31afb23d518d
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2025-08-11 18:43:45 +0300

    refactored defaultValues

--- code/src/containers/modals/add-link/AddLinkModal.consts.ts
+++ code/src/containers/modals/add-link/AddLinkModal.consts.ts
@@ -1,5 +1,6 @@
 type AddLinkFormValues = { photoURL: string };
 
-export const defaultValues: AddLinkFormValues = {
+export const defaultValues: Readonly<AddLinkFormValues> = Object.freeze({
   photoURL: ""
-};
+});
+
