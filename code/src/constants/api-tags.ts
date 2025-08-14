cc31498fda86c79138f048c1947005f29dcf8ca6
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2025-08-14 12:00:06 +0300

    fixed typo

--- code/src/constants/api-tags.ts
+++ code/src/constants/api-tags.ts
@@ -11,8 +11,8 @@ export const rtkQueryTags = {
   USER_PROFILE: "USER_PROFILE",
   VIEW_HISTORY: "VIEW_HISTORY",
   WISHLIST: "WISHLIST",
-  ADDRESSES: "ADDRESSES",,
-  USER_PHOTO: "USER_PHOTO",
+  ADDRESSES: "ADDRESSES",
+  USER_PHOTO: "USER_PHOTO"
 } as const;
 
 export const rtkQueryTagsArray = Object.values(rtkQueryTags);
