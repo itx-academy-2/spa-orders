85dbee7ab19ebb87cc6de8a91ca9121674003f1e
Sofiia Yevush <syevu@softserveinc.com>
2025-11-10 13:29:22 +0200

    Added tests for MyReservations

---
+++ code/src/containers/user-account/my-reservations/MyReservations.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import { useSearchParams } from "react-router-dom";
+
+import MyReservations from "@/containers/user-account/my-reservations/MyReservations";
+
+const mockedUseSearchParams = useSearchParams as jest.Mock;
+
+jest.mock("react-router-dom", () => {
+    const actual = jest.requireActual("react-router-dom");
+    return {
+        ...actual,
+        useSearchParams: jest.fn(),
+    };
+});
+
+const renderAndMock = ({ mockSortOption }: { mockSortOption?: string } = {}) => {
+    const searchParams = new URLSearchParams();
+    if (mockSortOption) searchParams.set("sort", mockSortOption);
+
+    const setParams = jest.fn();
+    mockedUseSearchParams.mockReturnValue([searchParams, setParams]);
+
+    render(<MyReservations />);
+
+    return {
+        setParams,
+        searchParams,
+    };
+};
+
+describe("MyReservations Page", () => {
+    test("renders the title typography", () => {
+        renderAndMock();
+
+        expect(screen.getByText("MyReservations.title")).toBeInTheDocument();
+    });
+
+    test("renders dropdown component", () => {
+        renderAndMock();
+
+        expect(screen.getByTestId("app-dropdown")).toBeInTheDocument();
+    });
+});
--- code/src/pages/product-details/components/reserve-button/ReserveButton.test.tsx
+++ code/src/pages/product-details/components/reserve-button/ReserveButton.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import ReserveButton from "./ReserveButton";
+import ReserveButton from "@/pages/product-details/components/reserve-button/ReserveButton";
 
 describe("ReserveButton", () => {
   test("renders a button", () => {
     render(<ReserveButton />);
-    
+
     const reserveButton = screen.getByRole("button");   
     expect(reserveButton).toBeInTheDocument();
   });
