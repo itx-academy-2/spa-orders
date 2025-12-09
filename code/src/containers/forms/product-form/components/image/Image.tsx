f1f499340d87d70b57ee7e7d47cc30ddf3313e28
Sofiia Yevush <syevu@softserveinc.com>
2025-12-09 13:16:27 +0200

    Added button, modal and translations to Image field

--- code/src/containers/forms/product-form/components/image/Image.tsx
+++ code/src/containers/forms/product-form/components/image/Image.tsx
@@ -5,10 +5,14 @@ import {
   ProductFormImageSectionProps,
   ProductFormValues
 } from "@/containers/forms/product-form/ProductForm.types";
+import ImageSearchModal from "@/containers/modals/image-search-modal/ImageSearchModal";
 
 import AppBox from "@/components/app-box/AppBox";
 import AppInput from "@/components/app-input/AppInput";
 import AppTypography from "@/components/app-typography/AppTypography";
+import AppButton from "@/components/app-button/AppButton";
+
+import { useModalContext } from "@/context/modal/ModalContext";
 
 type ControllerRenderFunctionProps = {
   field: ControllerRenderProps<ProductFormValues, "image">;
@@ -16,6 +20,7 @@ type ControllerRenderFunctionProps = {
 
 const Image = ({ errors, control }: ProductFormImageSectionProps) => {
   const [isError, setIsError] = useState(false);
+  const { openModal } = useModalContext();
 
   const handleImgError = () => {
     setIsError(true);
@@ -75,6 +80,13 @@ const Image = ({ errors, control }: ProductFormImageSectionProps) => {
             onChange={handleChange}
             {...handlers}
           />
+          <AppButton onClick={() => openModal(
+            <ImageSearchModal
+              onSelect={(url) => onChange(url)}
+            />
+          )}>
+            <AppTypography translationKey="productForm.image.searchImageButton" />
+          </AppButton>
         </AppBox>
       </AppBox>
     );
