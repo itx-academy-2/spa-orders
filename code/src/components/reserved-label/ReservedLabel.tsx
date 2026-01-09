56bd257ba7ad4abff02cd0ce409bb34c62be42c4
Sofiia Yevush <syevu@softserveinc.com>
2026-01-09 15:36:38 +0200

    Created reserved label

---
+++ code/src/components/reserved-label/ReservedLabel.tsx
@@ -0,0 +1,27 @@
+import AppBox from "@/components/app-box/AppBox";
+import AppTypography from "@/components/app-typography/AppTypography";
+
+import styled from "@emotion/styled";
+
+const StyledLabel = styled(AppBox)`
+  position: absolute;
+  background-color: #751fff;
+  color: #ffffff;
+  padding: 2px 8px;
+  border-radius: 15px;
+  font-size: 15px;
+  z-index: 1;
+`;
+
+const ReservedLabel = () => {
+  return (
+    <StyledLabel data-testid="reserved-label">
+      <AppTypography
+        variant="caption-small"
+        translationKey="productsTable.label.reserved"
+      />
+    </StyledLabel>
+  );
+}
+
+export default ReservedLabel;
